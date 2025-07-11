abad28ef2fb6d2482f6f2884fc297ed57076f16f
mariavitaliavc <mariavitaliavc@gmail.com>
2025-07-11 15:41:57 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,3 +4,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// Ajout spécial StackBlitz
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true, // permet l'exposition publique
+    port: 3000, // StackBlitz utilise le port 3000 automatiquement
+    strictPort: true,
+  },
+});
